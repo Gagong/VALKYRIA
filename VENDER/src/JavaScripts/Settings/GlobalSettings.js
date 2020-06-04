0e4a3ddb946571b5516845ecefe41b4225e250e2
Gagong <belyaevroma@yandex.ru>
2020-06-04 23:29:48 +0300

    GlobalSettings fix

--- VENDER/src/JavaScripts/Settings/GlobalSettings.js
+++ VENDER/src/JavaScripts/Settings/GlobalSettings.js
@@ -1,39 +1,34 @@
 class GlobalSettings {
   constructor() {
-    let self = this;
-    chrome.storage.local.get({
-      headerColor: "#191919",
-      headerOpacity: "0.9",
-      windowColor: "#191919",
-      windowOpacity: "0.8",
-      timerTick: 500,
-      windowsToTabs: false,
-    }, items => {
-      self._settings = items;
-    });
+   this._headerColor = "#191919";
+   this._headerOpacity = "0.9"
+   this._windowColor = "#191919";
+   this._windowOpacity = "0.8";
+   this._timerTick = 500;
+   this._windowsToTabs = false;
   }
 
   get headerColor() {
-    return this._settings.headerColor;
+    return this._headerColor;
   }
 
   get headerOpacity() {
-    return this._settings.headerOpacity;
+    return this._headerOpacity;
   }
 
   get windowColor() {
-    return this._settings.windowColor;
+    return this._windowColor;
   }
 
   get windowOpacity() {
-    return this._settings.windowOpacity;
+    return this._windowOpacity;
   }
 
   get timerTick() {
-    return this._settings.timerTick;
+    return this._timerTick;
   }
 
   get windowsToTabs() {
-    return this._settings.windowsToTabs;
+    return this.windowsToTabs;
   }
 }
